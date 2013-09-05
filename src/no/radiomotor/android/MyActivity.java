150ecd406686d6aba86c562d0ef5023a148363b9
SindreNordbo <sindre.nordbo@bekk.no>
2013-09-05 19:33:22 +0200

    Added AndroidAnnotations

--- src/no/radiomotor/android/MyActivity.java
+++ src/no/radiomotor/android/MyActivity.java
@@ -1,15 +1,10 @@
 package no.radiomotor.android;
 
 import android.app.Activity;
-import android.os.Bundle;
+import com.googlecode.androidannotations.annotations.EActivity;
 
+@EActivity(R.layout.main)
 public class MyActivity extends Activity {
-	/**
-	 * Called when the activity is first created.
-	 */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-	}
+
+
 }
