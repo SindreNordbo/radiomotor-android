6e70b43a44b48bae8108333fef2e0747ff9cf3d9
SindreNordbo <sindre.nordbo@bekk.no>
2013-09-08 13:25:01 +0200

    Get RSS-feed and display in a list

--- src/no/radiomotor/android/MyActivity.java
+++ src/no/radiomotor/android/MyActivity.java
@@ -1,6 +1,15 @@
 package no.radiomotor.android;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import com.googlecode.androidannotations.annotations.AfterViews;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Environment;
@@ -11,14 +20,29 @@ import com.googlecode.androidannotations.annotations.EActivity;
 import com.googlecode.androidannotations.annotations.OnActivityResult;
 import com.googlecode.androidannotations.annotations.OptionsItem;
 import com.googlecode.androidannotations.annotations.OptionsMenu;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+import static no.radiomotor.android.RadiomotorXmlParser.Item;
 
 import java.io.File;
 
 @EActivity(R.layout.main)
 @OptionsMenu(R.menu.main)
-public class MyActivity extends Activity {
-    private final int PICTURE_REQUEST_CODE = 1;
-    private final String IMAGE_PATH = Environment.getExternalStorageDirectory()+File.separator + "radiomotor.jpg";
+public class MyActivity extends ListActivity {
+
+	private final int PICTURE_REQUEST_CODE = 1;
+	private final String IMAGE_PATH = Environment.getExternalStorageDirectory()+File.separator + "radiomotor.jpg";
+
+	@AfterViews
+	void getRss() {
+		new DownloadXmlTask().execute("http://www.radiomotor.no/feed/");
+	}
 
     @OptionsItem(R.id.action_picture)
     public void cameraSelected() {
@@ -48,4 +72,78 @@ public class MyActivity extends Activity {
         }
     }
 
+	private class DownloadXmlTask extends AsyncTask<String, Void, List<Item>> {
+		@Override
+		protected List<Item> doInBackground(String... urls) {
+			try {
+				return loadXmlFromNetwork(urls[0]);
+			} catch (IOException e) {
+				Log.e(getClass().getSimpleName(), e.getLocalizedMessage(), e);
+				return null; // TODO
+			} catch (XmlPullParserException e) {
+				Log.e(getClass().getSimpleName(), e.getLocalizedMessage(), e);
+				return null; // TODO
+			}
+		}
+
+		@Override
+		protected void onPostExecute(List<Item> result) {
+			Log.d(getClass().getSimpleName(), result.toString());
+			getListView().setAdapter(new NewsFeedAdapter(getApplicationContext(), R.layout.row_newsitem, result));
+		}
+	}
+
+	private List<Item> loadXmlFromNetwork(String urlString) throws XmlPullParserException, IOException {
+		InputStream stream = null;
+		RadiomotorXmlParser stackOverflowXmlParser = new RadiomotorXmlParser();
+		List<Item> entries = null;
+		try {
+			stream = downloadUrl(urlString);
+			entries = stackOverflowXmlParser.parse(stream);
+		} finally {
+			if (stream != null) {
+				stream.close();
+			}
+		}
+
+		return entries;
+	}
+
+	private InputStream downloadUrl(String urlString) throws IOException {
+		URL url = new URL(urlString);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setReadTimeout(10000 /* milliseconds */);
+		conn.setConnectTimeout(15000 /* milliseconds */);
+		conn.setRequestMethod("GET");
+		conn.setDoInput(true);
+		conn.setRequestProperty("Content-Type", "text/plain; charset=utf-8");
+		conn.connect();
+		return conn.getInputStream();
+	}
+
+	public class NewsFeedAdapter extends ArrayAdapter {
+
+		private final int resource;
+		private final LayoutInflater inflater;
+		private final Context context;
+
+		public NewsFeedAdapter(final Context ctx, final int resourceId, final List<Item> objects) {
+			super(ctx, resourceId, objects);
+			resource = resourceId;
+			inflater = LayoutInflater.from(ctx);
+			context = ctx;
+		}
+
+		@Override
+		public View getView(final int position, View convertView, final ViewGroup parent) {
+			convertView = inflater.inflate(resource, null);
+
+			Item item = (Item) getItem(position);
+
+			TextView itemTitle = (TextView) convertView.findViewById(R.id.itemTitle);
+			itemTitle.setText(item.title);
+
+			return convertView;
+		}
+	}
 }
---
+++ src/no/radiomotor/android/RadiomotorXmlParser.java
@@ -0,0 +1,135 @@
+package no.radiomotor.android;
+
+import android.util.Xml;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RadiomotorXmlParser {
+
+	private String ns = null;
+
+	public List<Item> parse(InputStream stream) throws XmlPullParserException, IOException {
+		XmlPullParser parser = Xml.newPullParser();
+		try {
+			parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+			parser.setInput(stream, null);
+			parser.nextTag();
+			return readRss(parser);
+		} finally {
+			stream.close();
+		}
+	}
+
+	private List<Item> readRss(XmlPullParser parser) throws IOException, XmlPullParserException {
+		List<Item> entries = new ArrayList<Item>();
+
+		parser.require(XmlPullParser.START_TAG, ns, "rss");
+		while (parser.next() != XmlPullParser.END_TAG) {
+			if (parser.getEventType() != XmlPullParser.START_TAG) {
+				continue;
+			}
+			parser.require(XmlPullParser.START_TAG, ns, "channel");
+			while (parser.next() != XmlPullParser.END_TAG) {
+				if (parser.getEventType() != XmlPullParser.START_TAG) {
+					continue;
+				}
+				String name = parser.getName();
+				if (name.equals("item")) {
+					entries.add(readItem(parser));
+				} else {
+					skip(parser);
+				}
+			}
+		}
+		return entries;
+	}
+
+	public static class Item {
+		public String title;
+		public String link;
+		public String content;
+
+		public Item(String title, String link, String content) {
+			this.title = title;
+			this.link = link;
+			this.content = content;
+		}
+
+		@Override
+		public String toString() {
+			return "Item{" +
+					"title='" + title + '\'' +
+					", link='" + link + '\'' +
+					'}';
+		}
+	}
+
+	public Item readItem(XmlPullParser parser) throws IOException, XmlPullParserException {
+		parser.require(XmlPullParser.START_TAG, ns, "item");
+		String title = null;
+		String link = null;
+		String content = null;
+		while (parser.next() != XmlPullParser.END_TAG) {
+			if (parser.getEventType() != XmlPullParser.START_TAG) {
+				continue;
+			}
+			String name = parser.getName();
+			if (name.equals("title")) {
+				title = readTag(parser, "title");
+			} else if (name.equals("link")) {
+				link = readTag(parser, "link");
+			} else if (name.equals("content:encoded")) {
+				content = readContent(parser);
+			} else {
+				skip(parser);
+			}
+		}
+		return new Item(title, link, content);
+	}
+
+	private String readTag(XmlPullParser parser, String tag) throws IOException, XmlPullParserException {
+		parser.require(XmlPullParser.START_TAG, ns, tag);
+		String text = readText(parser);
+		parser.require(XmlPullParser.END_TAG, ns, tag);
+		return text;
+	}
+
+	private String readContent(XmlPullParser parser) throws IOException, XmlPullParserException {
+		parser.require(XmlPullParser.START_TAG, ns, "content:encoded");
+		String content = readText(parser);
+		// TODO remove HTML-tags etc
+		parser.require(XmlPullParser.END_TAG, ns, "content:encoded");
+		return content;
+	}
+
+	private String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
+		String result = "";
+		if (parser.next() == XmlPullParser.TEXT) {
+			result = parser.getText();
+			parser.nextTag();
+		}
+		return result;
+	}
+
+	private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
+		if (parser.getEventType() != XmlPullParser.START_TAG) {
+			throw new IllegalStateException();
+		}
+		int depth = 1;
+		while (depth != 0) {
+			switch (parser.next()) {
+				case XmlPullParser.END_TAG:
+					depth--;
+					break;
+				case XmlPullParser.START_TAG:
+					depth++;
+					break;
+			}
+		}
+	}
+}
