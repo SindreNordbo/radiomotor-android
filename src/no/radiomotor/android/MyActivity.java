6229d83bd74c25088b6471c30e4636af131a9146
SindreNordbo <sindre.nordbo@bekk.no>
2013-09-08 11:24:14 +0200

    Added actionbar

--- src/no/radiomotor/android/MyActivity.java
+++ src/no/radiomotor/android/MyActivity.java
@@ -2,8 +2,10 @@ package no.radiomotor.android;
 
 import android.app.Activity;
 import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.OptionsMenu;
 
 @EActivity(R.layout.main)
+@OptionsMenu(R.menu.main)
 public class MyActivity extends Activity {
 
 
