d1c3a652867a1c5a4a1f175910a0724c9feaf816
Christoffer Marcussen <christoffer.marcussen@bekk.no>
2013-09-19 21:49:55 +0200

    Choose between taking new photo and choosing photo from gallery

--- src/no/radiomotor/android/MyActivity.java
+++ src/no/radiomotor/android/MyActivity.java
@@ -1,10 +1,13 @@
 package no.radiomotor.android;
 
+import android.app.AlertDialog;
 import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.AudioManager;
@@ -32,10 +35,10 @@ import com.googlecode.androidannotations.annotations.OptionsItem;
 import com.googlecode.androidannotations.annotations.OptionsMenu;
 import com.googlecode.androidannotations.annotations.UiThread;
 import com.googlecode.androidannotations.annotations.ViewById;
-
 import com.menor.easyfacebookconnect.EasyActionListener;
 import com.menor.easyfacebookconnect.EasyLoginListener;
 import com.menor.easyfacebookconnect.ui.EasyFacebookFragmentActivity;
+
 import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.File;
@@ -47,38 +50,43 @@ import java.util.ArrayList;
 
 import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 import static android.widget.Toast.LENGTH_LONG;
-import static no.radiomotor.android.RadioService.*;
+import static no.radiomotor.android.RadioService.ACTION_PLAY;
+import static no.radiomotor.android.RadioService.ACTION_STARTED;
+import static no.radiomotor.android.RadioService.ACTION_STOP;
+import static no.radiomotor.android.RadioService.ACTION_STOPPED;
+import static no.radiomotor.android.RadioService.ACTION_STOPPED_ERROR;
 import static no.radiomotor.android.RadiomotorXmlParser.Item;
 
 @EActivity(R.layout.main)
 @OptionsMenu(R.menu.main)
 public class MyActivity extends EasyFacebookFragmentActivity {
     public static final String IS_RADIO_PLAYING_KEY = "isRadioPlaying";
-	private static final String TAG = "SBN";
-	private static final String PAGE_ID = "158209464386457";
-	private static final int NOTIFICATION_ID = 24;
+    private static final String TAG = "SBN";
+    private static final String PAGE_ID = "158209464386457";
+    private static final int NOTIFICATION_ID = 24;
+    private static final int GALLERY_REQUEST_CODE = 0;
+    private final int PICTURE_REQUEST_CODE = 1;
+    private final String IMAGE_PATH = Environment.getExternalStorageDirectory()+File.separator + "radiomotor.jpg";
 
-	@ViewById ListView newsFeedList;
+    @ViewById ListView newsFeedList;
     @ViewById TextView noNewsTextView;
 
-    private final String IMAGE_PATH = Environment.getExternalStorageDirectory()+File.separator + "radiomotor.jpg";
-    private final int PICTURE_REQUEST_CODE = 1;
     private CacheHelper cacheHelper;
     boolean isRadioPlaying;
     MenuItem refresh;
     MenuItem radioControl;
-	NotificationManager mNotificationManager;
-	private NotificationCompat.Builder notificationBuilder;
+    NotificationManager mNotificationManager;
+    private NotificationCompat.Builder notificationBuilder;
 
-	@Override
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         isRadioPlaying = SharedPreferencesHelper.get(this).getBoolean(IS_RADIO_PLAYING_KEY, false);
-		mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-	}
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+    }
 
     @AfterViews
-    void getRss() {
+    void afterViewsSetup() {
         cacheHelper = new CacheHelper(getApplicationContext());
         downloadNewsfeed();
 
@@ -92,12 +100,30 @@ public class MyActivity extends EasyFacebookFragmentActivity {
 
     @OptionsItem(R.id.action_picture)
     public void cameraSelected() {
-        File file = new File(IMAGE_PATH);
-
-        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
-
-        this.startActivityForResult(cameraIntent, PICTURE_REQUEST_CODE);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                MyActivity.this);
+        alertDialogBuilder.setTitle(getString(R.string.choose_picture_operation));
+        alertDialogBuilder
+                .setMessage(getString(R.string.picture_information))
+                .setCancelable(true)
+                .setPositiveButton(getString(R.string.take_new_photo),new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        File file = new File(IMAGE_PATH);
+                        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+                        startActivityForResult(cameraIntent, PICTURE_REQUEST_CODE);
+                    }
+                })
+                .setNegativeButton(getString(R.string.upload_from_gallery),new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        Intent galleryIntent = new Intent(
+                                Intent.ACTION_PICK,
+                                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE);
+                    }
+                });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
     }
 
     @OptionsItem(R.id.action_play)
@@ -147,61 +173,78 @@ public class MyActivity extends EasyFacebookFragmentActivity {
         isRadioPlaying = playing;
     }
 
-    @OnActivityResult(PICTURE_REQUEST_CODE)
-    void onResult(int resultCode, Intent data) {
+    @OnActivityResult(GALLERY_REQUEST_CODE)
+    void onGalleryResult(int resultCode, Intent data) {
         if (resultCode == RESULT_OK) {
-			if (!isConnected()) {
-				connect(new EasyLoginListener() {
-					@Override
-					public void onSuccess(Response response) {
-						uploadPicture();
-					}
+            Uri selectedImage = data.getData();
+            String[] filePaths = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePaths, null, null, null);
+            cursor.moveToFirst();
 
-					@Override
-					public void onError(FacebookRequestError error) {
-						Toast.makeText(MyActivity.this, getString(R.string.facebook_login_failed_message), Toast.LENGTH_LONG).show();
-					}
-				});
-			} else {
-				uploadPicture();
-			}
-		}
+            int columnIndex = cursor.getColumnIndex(filePaths[0]);
+            String path = cursor.getString(columnIndex);
+            cursor.close();
+
+            uploadPicture(path);
+        }
     }
 
-	void uploadPicture() {
-		Toast.makeText(MyActivity.this, getString(R.string.upload_picture_confirmation), Toast.LENGTH_LONG).show();
-		notificationBuilder = new NotificationCompat.Builder(MyActivity.this);
-		notificationBuilder.setContentTitle(getString(R.string.upload_notification_title));
-		notificationBuilder.setContentText(getString(R.string.upload_notification_in_progress_text));
-		notificationBuilder.setSmallIcon(R.drawable.ic_stat_av_play);
-		notificationBuilder.setProgress(0, 0, true);
-		mNotificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
-		final Bitmap bitmap = BitmapFactory.decodeFile(IMAGE_PATH);
-		shareImageToPage(PAGE_ID + "/photos", bitmap, new EasyActionListener() {
-			@Override
-			public void onSuccess(Response response) {
-				notificationBuilder.setContentText(getString(R.string.upload_notification_done_text))
-						.setProgress(0, 0, false);
-				mNotificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
-			}
+    @OnActivityResult(PICTURE_REQUEST_CODE)
+    void onCameraResult(int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (!isConnected()) {
+                connect(new EasyLoginListener() {
+                    @Override
+                    public void onSuccess(Response response) {
+                        uploadPicture(IMAGE_PATH);
+                    }
 
-			@Override
-			public void onError(FacebookRequestError error, Exception exception) {
-				notificationBuilder.setContentText(getString(R.string.upload_notification_error_text))
-						.setProgress(0, 0, false);
-				mNotificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
-			}
-		});
-	}
+                    @Override
+                    public void onError(FacebookRequestError error) {
+                        Toast.makeText(MyActivity.this, getString(R.string.facebook_login_failed_message), Toast.LENGTH_LONG).show();
+                    }
+                });
+            } else {
+                uploadPicture(IMAGE_PATH);
+            }
+        }
+    }
 
-	@Override
+    void uploadPicture(String path) {
+        Toast.makeText(MyActivity.this, getString(R.string.upload_picture_confirmation), Toast.LENGTH_LONG).show();
+        notificationBuilder = new NotificationCompat.Builder(MyActivity.this);
+        notificationBuilder.setContentTitle(getString(R.string.upload_notification_title));
+        notificationBuilder.setContentText(getString(R.string.upload_notification_in_progress_text));
+        notificationBuilder.setSmallIcon(R.drawable.ic_stat_av_play);
+        notificationBuilder.setProgress(0, 0, true);
+        mNotificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
+        final Bitmap bitmap = BitmapFactory.decodeFile(path);
+        shareImageToPage(PAGE_ID + "/photos", bitmap, new EasyActionListener() {
+            @Override
+            public void onSuccess(Response response) {
+                notificationBuilder.setContentText(getString(R.string.upload_notification_done_text))
+                        .setProgress(0, 0, false);
+                mNotificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
+            }
+
+            @Override
+            public void onError(FacebookRequestError error, Exception exception) {
+                notificationBuilder.setContentText(getString(R.string.upload_notification_error_text))
+                        .setProgress(0, 0, false);
+                mNotificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
+            }
+        });
+    }
+
+    @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
     }
 
     @Background
     void downloadNewsfeed() {
-		String urlString = "http://www.radiomotor.no/feed/";
+        String urlString = "http://www.radiomotor.no/feed/";
         InputStream stream = null;
         RadiomotorXmlParser parser = new RadiomotorXmlParser();
         ArrayList<Item> entries;
